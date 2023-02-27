5f55e15a516d84f2d32e8a445132e628fe6e39fe
Felix P <felix@saucelabs.com>
2023-02-27 11:56:45 +0100

    Consume HTTPS_PROXY var

--- src/download.ts
+++ src/download.ts
@@ -1,5 +1,5 @@
 import fsPromises from 'fs/promises';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import path from 'path';
 
 import gzip from './archive/gzip';
@@ -7,6 +7,7 @@ import zip from './archive/zip';
 import tar from './archive/tar';
 
 import { HttpOptions } from './index';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 type FileEntry = {
   data: Buffer;
@@ -24,12 +25,28 @@ async function downloadAndUnpack(url: URL, filepath: string, binary: string, opt
   return await save(found[0], binary);
 }
 
+function getHttpsProxyValue(): string | undefined {
+  for (const k of Object.keys(process.env)) {
+    if (k.toUpperCase() === "HTTPS_PROXY") {
+      return process.env[k];
+    }
+  }
+  return undefined;
+}
+
 async function download(url: URL, options: HttpOptions) {
-  return await axios.get(url.toString(), {
-    responseType: 'arraybuffer',
+  const opts: AxiosRequestConfig = {
     headers: options.headers,
-    httpsAgent: options.httpsAgent,
-  }).then((res) => {
+    responseType: 'arraybuffer',
+  };
+
+  const httpsProxy = getHttpsProxyValue();
+  if (httpsProxy) {
+    opts.httpsAgent = new HttpsProxyAgent(httpsProxy);
+    // Note: proxy needs to be sure that the agent is used.
+    opts.proxy = false;
+  }
+  return await axios.get(url.toString(), opts).then((res) => {
     return res.data;
   }).catch((err) => {
     throw new Error(`failed to download: ${err}`);
